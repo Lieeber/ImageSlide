d744594492750950de810b3a5d7796498b224cb1
李兵 <353890493@QQ.com>
2015-01-10 12:14:15 +0800

    m
    m

---
+++ src/main/java/com/loveplusplus/demo/image/ImagesURL.java
@@ -0,0 +1,13 @@
+package com.loveplusplus.demo.image;
+
+public class ImagesURL {
+    public final static String[] imageUrls = new String[]{
+            "http://a.hiphotos.baidu.com/image/pic/item/1b4c510fd9f9d72a80f8481ad62a2834349bbb1d.jpg",
+            "http://c.hiphotos.baidu.com/image/pic/item/adaf2edda3cc7cd987b16cde3b01213fb80e911c.jpg",
+            "http://c.hiphotos.baidu.com/image/pic/item/8cb1cb1349540923eff53b799058d109b3de491c.jpg",
+            "http://h.hiphotos.baidu.com/image/pic/item/810a19d8bc3eb1350c58efbca41ea8d3fd1f441d.jpg",
+            "http://d.hiphotos.baidu.com/image/pic/item/3bf33a87e950352a15759a015143fbf2b2118ba7.jpg",
+            "http://h.hiphotos.baidu.com/image/pic/item/b3119313b07eca809a8069d5922397dda144837f.jpg",
+            "http://e.hiphotos.baidu.com/image/pic/item/7dd98d1001e939013586dd0c78ec54e736d1967f.jpg"
+    };
+}
---
+++ src/main/java/com/loveplusplus/demo/image/MainActivity.java
@@ -0,0 +1,158 @@
+package com.loveplusplus.demo.image;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.HackyViewPager;
+import com.viewpagerindicator.PageIndicator;
+
+import uk.co.senab.photoview.PhotoView;
+
+public class MainActivity extends Activity {
+    //实例化ImageLoader
+    protected ImageLoader imageLoader = ImageLoader.getInstance();
+    DisplayImageOptions options;
+
+    HackyViewPager pager;
+    PageIndicator mIndicator;
+
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_image_pager);
+        //初始化ImageLoader
+        initImageLoader(this);
+        //获得所有的图片地址
+        String[] imageUrls = ImagesURL.imageUrls;
+        //设置ImageLoader加载图片的属性
+        options = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(R.drawable.ic_empty)
+                .showImageOnFail(R.drawable.ic_error)
+                .resetViewBeforeLoading(true)
+                .cacheOnDisc(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .bitmapConfig(Bitmap.Config.ARGB_8888)
+                .displayer(new FadeInBitmapDisplayer(300))
+                .build();
+        //使用的是HackViewPager,目的就是为了防止滑动的时候抛异常
+        pager = (HackyViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new ImagePagerAdapter(imageUrls));
+
+        mIndicator = (CirclePageIndicator) findViewById(R.id.indicator);
+        //指针绑定ViewPager
+        mIndicator.setViewPager(pager);
+    }
+
+    public void initImageLoader(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .writeDebugLogs() // Remove for release app
+                .build();
+        imageLoader.init(config);
+    }
+
+    private class ImagePagerAdapter extends PagerAdapter {
+
+        private String[] images;
+        private LayoutInflater inflater;
+
+
+        ImagePagerAdapter(String[] images) {
+            this.images = images;
+            inflater = getLayoutInflater();
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            (container).removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            View imageLayout = inflater.inflate(R.layout.item_pager_image, view, false);
+
+            PhotoView imageView = (PhotoView) imageLayout.findViewById(R.id.image);
+            final ProgressBar spinner = (ProgressBar) imageLayout.findViewById(R.id.loading);
+
+            imageLoader.displayImage(images[position], imageView, options, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    spinner.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    String message = null;
+                    switch (failReason.getType()) {
+                        case IO_ERROR:
+                            message = "Input/Output error";
+                            break;
+                        case DECODING_ERROR:
+                            message = "Image can't be decoded";
+                            break;
+                        case NETWORK_DENIED:
+                            message = "Downloads are denied";
+                            break;
+                        case OUT_OF_MEMORY:
+                            message = "Out Of Memory error";
+                            break;
+                        case UNKNOWN:
+                            message = "Unknown error";
+                            break;
+                    }
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                    spinner.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    spinner.setVisibility(View.GONE);
+                }
+            });
+            (view).addView(imageLayout, 0);
+            return imageLayout;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view.equals(object);
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+    }
+}
